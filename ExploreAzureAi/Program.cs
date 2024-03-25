4649d0aa19997b311d3681a24c3166b82bbaebc9
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2024-03-25 10:40:58 +0700

    feat: add vision (#4)
    * feat: add vision
    
    * feat: Add vision
    
    remove key

---
+++ ExploreAzureAi/Program.cs
@@ -0,0 +1,40 @@
+using ExploreAzureAi.Components;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var key = builder.Configuration["Vision:ApiKey"];
+var endpoint = builder.Configuration["Vision:Endpoint"];
+if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(endpoint))
+{
+    var computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+    {
+        Endpoint = endpoint
+    };
+    builder.Services.AddSingleton<IComputerVisionClient>(computerVisionClient);
+}
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
